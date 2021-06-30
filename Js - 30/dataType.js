73551a1e425bf1243abd21ea32f6b84c798fe2c6
VINADES.,JSC <contact@vinades.vn>
2021-06-30 17:44:12 +0700

    bài tập về DOM

---
+++ Js - 30/dataType.js
@@ -0,0 +1,33 @@
+// show = document.getElementsByClassName("text")
+// console.log(show);
+
+// for (let i = 0; i < show.lenght; i++) {
+
+//     show[i].addEventListener('click', () => {
+//         show[i].style.color = "red"
+//     })
+// }
+
+// pElement.style.background = "red";
+// pElement = document.getElementById('text')
+
+// pElement_1 = document.getElementById('text-1')
+// console.log(pElement);
+
+// pElement_p = document.getElementsByTagName('p')
+// console.log(pElement_p);
+
+// pElement_1.addEventListener('click', () => pElement.style.background = "yellow")
+
+// pElement.addEventListener('click', () => {
+//     pElement.remove();
+// })
+
+// pElement.addEventListener('click', () => {
+//     console.log('click');
+// })
+
+function myFuntion() {
+    element = document.getElementById('content').value
+    document.getElementById('reslut').innerHTML = element
+}
